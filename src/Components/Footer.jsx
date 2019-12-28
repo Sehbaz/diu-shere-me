2aafc4fd889c986dcf96645cc4fcea06dfb7eadc
Sehbaz Rafik <sehbazm@gmail.com>
2019-12-28 21:58:25 +0000

    responsiveness update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,13 +3,29 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="main-container">
-      <h1 className="Footer-heading"> About us</h1>
-      <p>
-        Diu town sits at the eastern end of the island. The northern side of the
-        island, facing Gujarat, is tidal marsh and salt pans, while the southern
-        coast alternates between limestone cliffs, rocky coves and sandy
-        beaches, better for people-watching than sun worshipping.
-      </p>
+      <div className="about">
+        <h1 className="Footer-heading"> About Us</h1>
+        <p>
+          Diu town sits at the eastern end of the island. The northern side of
+          the island, facing Gujarat, is tidal marsh and salt pans, while the
+          southern coast alternates between limestone cliffs, rocky coves and
+          sandy beaches, better for people-watching than sun worshipping.
+        </p>
+        <p>Wedding Photographer and Cinematographer in Diu.</p>
+        <p>
+          We specialised in Gujarati, Sikh, Hindu Panjabi, Islamic and English
+          Wedding Photography and Cinematography.
+        </p>
+        <p>
+          Also specialised in Commercial Photography and Video, Corporate
+          Headshots, Business Advert Video.
+        </p>
+        <p>
+          Contact us to discuss more about your photo & video requirements, We
+          would love to hear from you. Give us a call.
+        </p>
+        <button class="button button1">0123456789</button>
+      </div>
     </div>
   );
 }
