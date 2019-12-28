10cd7bb53a69809446ef8e371566e14f8ba818e7
Sehbaz Rafik <sehbazm@gmail.com>
2019-12-28 14:24:32 +0000

    animation added

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -14,17 +14,25 @@ function Gallery() {
     <div className="image-container">
       <div className="row">
         <div className="column">
-          <img src={animation} style={{ width: "100%" }}></img>
+          <img src={animation} style={{ width: "100%" }} className="pic"></img>
+          <h3>Animation and Editing</h3>
+
           <img src={baby} style={{ width: "100%" }}></img>
+          <h3>Baby Shoot</h3>
         </div>
         <div className="column">
           <img src={hotel} style={{ width: "100%" }}></img>
+          <h3>Hotel Booking</h3>
           <img src={documentery} style={{ width: "100%" }}></img>
+          <h3>Shoot a Documentary</h3>
         </div>
         <div className="column">
           <img src={wedding} style={{ width: "100%" }}></img>
+          <h3>Wedding</h3>
           <img src={fashion} style={{ width: "100%" }}></img>
+          <h3>Fashion</h3>
           <img src={prewedding} style={{ width: "100%" }}></img>
+          <h3>Pre-Wedding Shoot</h3>
         </div>
       </div>
     </div>
