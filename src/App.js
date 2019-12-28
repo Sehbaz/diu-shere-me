2df517435992f64daded54805473d74ded51b014
Sehbaz Rafik <sehbazm@gmail.com>
2019-12-28 13:13:32 +0000

    layout made responsive and resize image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import sea from "./images/sea.png";
 import "./App.css";
 import Gallery from "./Components/Gallery";
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={sea} className="App-logo" alt="logo" />
         <h1 className="App-heading">DiuSeherMe</h1>
       </header>
       <Gallery />
--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,45 +1,30 @@
 import React from "react";
-import img1 from "../images/img-1.jpg";
-import img2 from "../images/img-2.jpg";
-import img3 from "../images/img-3.jpg";
-import img4 from "../images/img-4.jpg";
-import img5 from "../images/img-5.jpg";
-import img6 from "../images/img-6.jpg";
+import animation from "../images/animation.jpg";
+import baby from "../images/baby.jpg";
+import documentery from "../images/documentery.jpg";
+import fashion from "../images/fashion.jpg";
+import hotel from "../images/hotel.jpg";
+import prewedding from "../images/pre-wedding.jpg";
 import ImageContentHover from "react-image-hover";
+import wedding from "../images/wedding.jpg";
 import "./Gallery.css";
 
 function Gallery() {
   return (
-    <div>
+    <div className="image-container">
       <div className="row">
         <div className="column">
-          <div className="img-container">
-            <img src={img6} style={{ width: "100%" }} className="image"></img>
-            <div className="overlay">
-              <div className="text">John Doe</div>
-            </div>
-          </div>
-
-          <img src={img2} style={{ width: "100%" }}></img>
-          <ImageContentHover
-            className="image-cover image"
-            tileWidth="200px"
-            tileHeight="200px"
-            image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/example-image.jpg"
-            content={{
-              title: "Image Title",
-              body: "Image content viewer"
-            }}
-            effect="fadeIn"
-          />
+          <img src={animation} style={{ width: "100%" }}></img>
+          <img src={baby} style={{ width: "100%" }}></img>
         </div>
         <div className="column">
-          <img src={img3} style={{ width: "100%" }}></img>
-          <img src={img4} style={{ width: "100%" }}></img>
+          <img src={hotel} style={{ width: "100%" }}></img>
+          <img src={documentery} style={{ width: "100%" }}></img>
         </div>
         <div className="column">
-          <img src={img5} style={{ width: "100%" }}></img>
-          <img src={img6} style={{ width: "100%" }}></img>
+          <img src={wedding} style={{ width: "100%" }}></img>
+          <img src={fashion} style={{ width: "100%" }}></img>
+          <img src={prewedding} style={{ width: "100%" }}></img>
         </div>
       </div>
     </div>
