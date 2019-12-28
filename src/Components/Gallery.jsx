41cfd94a32a0b8dc103f3ca7ef3b87003a2fc4a0
Sehbaz Rafik <sehbazm@gmail.com>
2019-12-28 19:04:46 +0000

    particleJS add and layout update

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -5,7 +5,6 @@ import documentery from "../images/documentery.jpg";
 import fashion from "../images/fashion.jpg";
 import hotel from "../images/hotel.jpg";
 import prewedding from "../images/pre-wedding.jpg";
-import ImageContentHover from "react-image-hover";
 import wedding from "../images/wedding.jpg";
 import "./Gallery.css";
 
@@ -14,25 +13,72 @@ function Gallery() {
     <div className="image-container">
       <div className="row">
         <div className="column">
-          <img src={animation} style={{ width: "100%" }} className="pic"></img>
-          <h3>Animation and Editing</h3>
-
-          <img src={baby} style={{ width: "100%" }}></img>
-          <h3>Baby Shoot</h3>
+          <div className="container">
+            <img src={animation} alt="Animation" className="image"></img>
+            <div className="overlay">
+              <div className="text">
+                <p>Editing</p>
+                <p>and</p>
+                <p>Animation</p>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <img src={baby} alt="Baby"></img>
+            <div className="overlay">
+              <div className="text">
+                {" "}
+                <p>Baby Shoot</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="column">
-          <img src={hotel} style={{ width: "100%" }}></img>
-          <h3>Hotel Booking</h3>
-          <img src={documentery} style={{ width: "100%" }}></img>
-          <h3>Shoot a Documentary</h3>
+          <div className="container">
+            <img src={wedding} alt="Wedding" className="image"></img>
+            <div className="overlay">
+              <div className="text">
+                <p>Wedding</p>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <img src={fashion} alt="Fashion"></img>
+            <div className="overlay">
+              <div className="text">
+                {" "}
+                <p>Fashion</p>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <img src={documentery} alt="Documentery"></img>
+            <div className="overlay">
+              <div className="text">
+                {" "}
+                <p>Documentery</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="column">
-          <img src={wedding} style={{ width: "100%" }}></img>
-          <h3>Wedding</h3>
-          <img src={fashion} style={{ width: "100%" }}></img>
-          <h3>Fashion</h3>
-          <img src={prewedding} style={{ width: "100%" }}></img>
-          <h3>Pre-Wedding Shoot</h3>
+          <div className="container">
+            <img src={prewedding} alt="PreWedding" className="image"></img>
+            <div className="overlay">
+              <div className="text">
+                <p>PreWedding</p>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <img src={hotel} alt="HotelBooking"></img>
+            <div className="overlay">
+              <div className="text">
+                {" "}
+                <p>Hotel Booking</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
